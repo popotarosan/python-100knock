2e9c84b746f85fd9b4f27a93cecf44ee4572e440
nomujun <nomujun@nomujunnoMacBook-Pro.local>
2019-11-04 22:34:15 +0900

    [fix]practice-05.py

--- practice-05.py
+++ practice-05.py
@@ -4,9 +4,9 @@
 class argument_error(Exception):
     pass
 
-def make_n_gram(sequence,word_count):
+def make_character_n_gram(sequence,word_count):
     """
-    与えられたシーケンス（文字列やリストなど）からn-gramを作る
+    与えられたシーケンス（文字列やリストなど）から文字単位のn-gramを作る
     """
     try:
         if  type(sequence) is str:
@@ -24,9 +24,34 @@ def make_n_gram(sequence,word_count):
         return sequence
     return n_gram
 
+def make_word_n_gram(sequence,word_count):
+    """
+    与えられたシーケンス（文字列やリストなど）から単語単位のn-gramを作る
+    """
+    try:
+        if  type(sequence) is str:
+            # 文字列は空白で区切られている前提
+            print(sequence)
+            sequence_arr = sequence.split()
+            print(sequence_arr)
+            n_gram = [''.join(sequence_arr[index:index+word_count]) for index,word in enumerate(sequence_arr) ]
+
+        elif type(sequence) is list:
+            n_gram = [''.join(sequence[index:index+word_count]) for index,word in enumerate(sequence) ]
+
+        else:
+            raise argument_error
+    except argument_error:
+        print("引数の型が間違っています")
+        return sequence
+    return n_gram
 if __name__ == "__main__":
-    print("n-gramを作成します")
+    print("文字単位n-gramを作成します")
     input_text = input("分割したい文字列を入力してください")
-    word_count = int(input("文字数を入力してください"))
+    word_count = int(input("分割単位を入力してください"))
+    print("文字単位n-gram\n",make_character_n_gram(input_text,word_count))
     
-    print(make_n_gram(input_text,word_count))
+    print("単語単位n-gramを作成します")
+    input_text = input("分割したい文字列を入力してください")
+    word_count = int(input("分割単位を入力してください"))
+    print("単語単位n-gram\n",make_word_n_gram(input_text,word_count))
