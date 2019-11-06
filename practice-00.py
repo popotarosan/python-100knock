9ebf7e59b14c84e1a84aa82115b6638e2e7b22be
nomujun <nomujun@nomujunnoMacBook-Pro.local>
2019-11-07 00:49:27 +0900

    back to start

--- practice-00.py
+++
@@ -1,5 +0,0 @@
-# 00. 文字列の逆順
-# 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
-
-string = "stressed"
-print(string[::-1])
--- practice-01.py
+++
@@ -1,8 +0,0 @@
-# 01. 「パタトクカシーー」
-# 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
-
-string = "パタトクカシーー"
-
-stringslice = string[0]+string[2]+string[4]+string[6]
-
-print(stringslice)
