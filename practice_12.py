4b4e72d41eadcb2d1a6069fc3328acd92b1b1faf
nomujun <nomujun@nomujunnoMacBook-Pro.local>
2019-11-08 15:48:55 +0900

    [add]practice_12

---
+++ practice_12.py
@@ -0,0 +1,13 @@
+# 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
+# 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+
+import pandas as pd 
+
+if __name__ == '__main__':
+    dataset = pd.read_table("hightemp.txt",header=None)
+    print(dataset)
+    dataset1 = dataset.iloc[:,0]
+    dataset1.to_csv('col1.txt',header=False,index=False)
+    dataset1 = dataset.iloc[:,1]
+    dataset1.to_csv('col2.txt',header=False,index=False)
+    # print(dataset.iloc[:,0:1])
